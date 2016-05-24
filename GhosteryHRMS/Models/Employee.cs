5730ef9a693aab0f986ad02e239830404bc78002
Chad Cloward <Chad Cloward>
2016-05-24 15:58:03 -0600

    Added Employee data model.

---
+++ GhosteryHRMS/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhosteryHRMS.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int SSN { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Telephone { get; set; }
+        public string Email { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public double AnnualSalary { get; set; }
+        public string Department { get; set; }
+        public string Supervisor { get; set; }
+        public DateTime HireDate { get; set; }
+        
+    }
+}
