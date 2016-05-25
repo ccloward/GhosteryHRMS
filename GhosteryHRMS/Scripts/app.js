3e33206fed37234452a40112db8a8eb6e11ac42a
Chad Cloward <Chad Cloward>
2016-05-24 18:09:20 -0600

    Added Knockout.js libraries for the view layer. Created initial databinding framework.

---
+++ GhosteryHRMS/Scripts/app.js
@@ -0,0 +1,31 @@
+﻿var ViewModel = function () {
+
+    this.employees = ko.observableArray();
+    this.error = ko.observable();
+
+    var employeesUri = '/api/Employees/';
+
+    function ajaxHelper(uri, method, data) {
+        this.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            this.error(errorThrown);
+        });
+    }
+
+    function getEmployees() {
+        ajaxHelper(employeesUri, 'GET').done(function (data) {
+            this.employees(data);
+        });
+    }
+
+    // Fetch the initial data.
+    getEmployees();
+};
+
+ko.applyBindings(new ViewModel());
